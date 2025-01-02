9fdc9f184ef4e0b569cc18d0edc32694d320443b
4lias <jkartkowski@gmail.com>
2025-01-02 03:08:46 +0100

    podstawowy system platformowy

---
+++ rgb_windows/scripts/player.gd
@@ -0,0 +1,42 @@
+extends CharacterBody2D
+
+
+@export var speed = 300.0
+@export var jump_velocity = 400.0
+
+func _ready() -> void:
+	pass
+	
+func _physics_process(delta: float) -> void:
+	$AnimatedSprite2D.play()
+	
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if is_on_floor():
+		if Input.is_action_just_pressed("jump"):
+			velocity.y = -jump_velocity
+			$AnimatedSprite2D.animation = "jump"
+		if velocity.x != 0:
+			$AnimatedSprite2D.animation = "walk"
+		if velocity.x == 0:
+			$AnimatedSprite2D.animation = "idle"
+	else:
+		$AnimatedSprite2D.animation = "fall"
+	
+	if velocity.x > 0:
+		$AnimatedSprite2D.flip_h = false
+	if velocity.x < 0:
+		$AnimatedSprite2D.flip_h = true
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * speed
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		
+	move_and_slide()
